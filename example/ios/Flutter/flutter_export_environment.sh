4894b06e873b16880cdbf737090f42bffa00616f
maddyb99 <43785086+maddyb99@users.noreply.github.com>
2021-03-31 17:52:57 +0530

    bump up version for null safety migration

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/mac/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/mac/Documents/projects/flutter/plugins/flutter_feather_icons/example"
+export "FLUTTER_ROOT=/opt/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/maddyb/Documents/PROJECTS/Flutter/PACKAGES/flutter_feather_icons/example"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
